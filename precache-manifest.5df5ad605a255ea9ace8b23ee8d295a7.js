1082820984716ff1233bb3d40b8d7c6d3bbc5d6c
Chandan <dchandankrsingh@gmail.com>
2022-02-15 17:08:29 +0530

    Updates

---
+++ precache-manifest.5df5ad605a255ea9ace8b23ee8d295a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb9663e277a90ac9c3cbec218e79b82",
+    "url": "/Hacker-new/index.html"
+  },
+  {
+    "revision": "bfc9be0e17780264040d",
+    "url": "/Hacker-new/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/Hacker-new/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hacker-new/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc9be0e17780264040d",
+    "url": "/Hacker-new/static/js/main.d50e4c11.chunk.js"
+  },
+  {
+    "revision": "16a5ef347803286266dd",
+    "url": "/Hacker-new/static/js/runtime-main.0bfdf198.js"
+  }
+]);
